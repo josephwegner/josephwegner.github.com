4ab528b8f1cde00a3a16621d2adbd578d6ce1ed0
Joe Wegner <joe@wegnerdesign.com>
2012-12-22 21:50:55 -0600

    Lots of design changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,29 @@
+var loadInterval;
+
+$(document).ready(function() {
+	loadInterval = setInterval(function() {
+		var ele = $("#scrollArrow");
+
+		if($(ele).hasClass("a3")) {
+			$(ele).removeClass("a3");
+		} else if($(ele).hasClass("a2")) {
+			$(ele).removeClass("a2").addClass("a3");
+		} else if($(ele).hasClass("a1")) {
+			$(ele).removeClass("a1").addClass("a2");
+		} else {
+			$(ele).addClass("a1");
+		}
+	}, 500)
+});
+
 $(window).load(function() {
+
+	clearInterval(loadInterval);
+	$("#scrollArrow").attr('class', '');
+	$("body").removeClass("loading");
+	$("#loadOverlay").fadeOut(2000);
+
+	var parallaxSpeed = .4;
 
 	var parallaxWrap = $("<div>").addClass("parallax-wrap").css({
 		position: "absolute",
@@ -18,8 +43,6 @@ $(window).load(function() {
 		});
 
 		if($(this).attr('offset')) {
-			console.log($(this).offset().top);
-			console.log($(this).attr('offset'));
 
 			var offset = $(this).attr('offset');
 			if(offset.indexOf("%") > -1) {
@@ -28,16 +51,47 @@ $(window).load(function() {
 			}
 
 			$(this).css('top', "+="+offset);
-			console.log($(this).offset().top);
 		}
 
 		$(parallaxWrap).append(this);
-	})
+
+	});
 
 	$(window).scroll(function() {
-		var top = $(window).scrollTop() * .4;
+		var top = $(window).scrollTop() * parallaxSpeed;
 
 		$("#content").css('top', top);
+
+		if($("#nav").is(":visible") && $(window).scrollTop() <= 10) {
+			$("#nav").stop(true).fadeOut(500);
+		} else if(!$("#nav").is(":visible") && $(window).scrollTop() > 10) {
+			$("#nav").stop(true).fadeIn(500);
+		}
 	});
+
+	$("#nav a").click(function(e) {
+		e.preventDefault();
+
+		var target = $($(this).attr('href'));
+
+		var newTop
+
+		if($(target).hasClass("parallax")) {
+			newTop = $(target).offset().top
+		} else {
+			var cur = $(window).scrollTop();
+			var dest = $(target).offset().top;
+
+			var change = (dest - cur) * (1 - parallaxSpeed);
+			console.log(dest, change)
+			newTop = dest + change;
+		}
+
+		$('html, body').animate({
+			scrollTop: newTop
+		}, 2000)
+	});
+
+	$("#scrollArrow").click(function() { $("#goAbout").click(); })
 
 });
