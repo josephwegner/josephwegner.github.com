98b3b4699b45ff7cbcc1d8322b0e93b592c4ec8c
Joe Wegner <joe@wegnerdesign.com>
2012-12-24 08:37:00 -0600

    nav changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,7 @@
+var navItems = [];
 
 $(window).load(function() {
-	console.log("hit");
+
 	var parallaxSpeed = .4;
 
 	var parallaxWrap = $("<div>").addClass("parallax-wrap").css({
@@ -9,6 +10,22 @@ $(window).load(function() {
 		left: 0,
 		width: "100%"
 	});
+
+
+
+	$("#nav ul li a").each(function() {
+		var ele = $($(this).attr('href'));
+
+		if(ele) {
+			navItems.push({
+				ele: ele,
+				nav: this
+			});
+		}
+	})
+
+	selectedNav = 0
+	$(navItems[0].nav).addClass("selected");
 
 	if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
 		$("body").append(parallaxWrap);
@@ -46,6 +63,9 @@ $(window).load(function() {
 			} else if(!$("#nav").is(":visible") && $(window).scrollTop() > 10) {
 				$("#nav").stop(true).fadeIn(500);
 			}
+
+			checkNavItems();
+
 		});
 	}
 	$("#nav a").click(function(e) {
@@ -62,15 +82,42 @@ $(window).load(function() {
 			var dest = $(target).offset().top;
 
 			var change = (dest - cur) * (1 - parallaxSpeed);
-			console.log(dest, change)
+
 			newTop = dest + change;
 		}
 
 		$('html, body').animate({
 			scrollTop: newTop
-		}, 2000)
+		}, 2000, function() { checkNavItems(true); })
 	});
 
 	$("#scrollArrow").click(function() { $("#goAbout").click(); })
 
 });
+
+var beforeScroll = 0;
+var selectedNav = 0;
+
+function checkNavItems() {
+
+	var scroll = $(window).scrollTop();
+
+	var dir = scroll > beforeScroll ? "down" : "up";
+	beforeScroll = scroll;
+
+	if(dir === "down" && selectedNav < navItems.length - 1) {
+
+		if(scroll + 1>= $(navItems[selectedNav + 1].ele).offset().top) {
+			console.log("switch!");
+			selectedNav++;
+			$("#nav .selected").removeClass("selected");
+			$(navItems[selectedNav].nav).addClass("selected");
+		}
+	} else if(dir === "up" && selectedNav > 0) {
+		if(scroll - 1 <= $(navItems[selectedNav - 1].ele).offset().top) {
+			selectedNav--;
+			$("#nav .selected").removeClass("selected");
+			$(navItems[selectedNav].nav).addClass("selected");
+		}
+	}
+}
