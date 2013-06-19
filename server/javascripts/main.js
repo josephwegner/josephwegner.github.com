be1b62234cdf5c7e078be4e165c7a405c6ece53c
Joe Wegner <joe@wegnerdesign.com>
2013-06-18 19:23:51 -0500

    Analytics Platform is a Privacy Decision

---
+++ server/javascripts/main.js
@@ -0,0 +1,72 @@
+$(window).ready(function() {
+
+	var colors = ["blue", "red", "green", "orange", "purple"]
+
+    $(".blogpost").each(function() {
+    	var color = colors[Math.floor(Math.random() * colors.length)];
+    	$(this).addClass(color);
+    });
+    
+    var editors = {};
+
+    var typeIn = [];
+
+    $(".editor").each(function() {
+    	var id = $(this).attr('id');
+    	var mode = $(this).attr('lang');
+    	var text = $(this).text();
+
+    	editors[id] = ace.edit(id);
+    	editors[id].setTheme("ace/theme/chrome");
+    	editors[id].getSession().setMode("ace/mode/"+mode);
+
+    	var that = this;
+    	setTimeout(function() {
+    		$(that).height($(that).find(".ace_gutter-cell").last().position().top + 100);
+    		editors[id].setValue("");
+    	}, 10);
+
+    	typeIn.push({
+    		ele: $(this),
+    		text: text,
+    		editor: editors[id]
+    	});
+
+    });
+
+    $(window).scroll(function(e) {
+    	var bottom = $(window).scrollTop() + $(window).height();
+
+    	for(var i=0,max=typeIn.length; i<max; i++) {
+    		if(bottom > $(typeIn[i].ele).offset().top + 100) {
+    			typeInEditor(typeIn.splice(i, 1)[0]);
+    			i--;
+    			max--;
+    		}
+    	}
+    });
+
+    $("#scrollArrow").click(function() {
+    	$("html, body").animate({
+    		scrollTop: $("#aboutme").offset().top
+    	}, 1000);
+    })
+
+});
+
+function typeInEditor(obj) {
+	var len = obj.text.length;
+	var time = 2000 / len;
+	time = time > 200 ? 200 : time;
+	console.log(time, len);
+
+	var type = setInterval(function() {
+		if(obj.text.length <= 0) {
+			clearInterval(type);
+		} else {
+			obj.editor.setValue(obj.editor.getValue()+obj.text.charAt(0));
+			obj.text = obj.text.substr(1);
+			obj.editor.gotoLine(0);
+		}
+	}, time)
+}
